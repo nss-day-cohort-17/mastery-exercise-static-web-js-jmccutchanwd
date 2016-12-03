86c260802e5068ddcccdb7e6446abf6f5c6c33e4
John McCutchan <john.mccutchan.wd@gmail.com>
2016-12-02 22:58:31 -0600

    tree is correct in console, worked out adding 7 to height

--- quiz.js
+++ quiz.js
@@ -39,77 +39,25 @@ button.addEventListener("click", getData);// grow your tree button listener
 function getData(){
   var symbol = document.getElementById("symbol").value + " ";
   var height = document.getElementById("height").value;
-  treeData.symbol = symbol;
-  treeData.height = height;
-  console.log("Inputted data: " + symbol + " " + height);
-  console.log(treeData); // see if object is set up
-  treeBuild(symbol, height); // pass object.symbol and .height to treeBuild
+  treeData.symbol = symbol; // passes symbol and it's value into obj treeData()
+  treeData.height = height; // passes height and it's value into obj treeData()
+  console.log(treeData); // **** test data **** displays object contents ****
+  treeBuild(treeData.symbol, treeData.height); // pass object.symbol and .height to treeBuild
 }
-function treeBuild(symbol, height) {
-    var conResult = "";
-    var htmlResult = "";
-    var spacer = " ";
-    var i = 0;
-    var j = height;
+function treeBuild(sym, hgt) { // pass treeData.symbol, treeData.height
+    var conResult = ""; // emtpy container to hold console line
+    var htmlResult = ""; // empty container to hold html line
+    var spacer = " "; // empty space that serves as spacer
+    var modCounter = Number(hgt) + 7; // turns hgt into a number and adds 7
+    var i = 1; // symbol counter
+    var j = modCounter; // spacer counter
     do {
-      i += 1;
-      j -= 1;
-      conResult = spacer.repeat(j) + symbol.repeat(i);
-      htmlResult += "<li>" + symbol.repeat(i) + "</li>";
-      document.getElementById("displayTree").innerHTML = htmlResult;
-      console.log(conResult)
-      console.log(spacer);
-} while (i < height);
-;
+      conResult = spacer.repeat(j) + sym.repeat(i); // builds line for console
+      htmlResult += "<li>" + sym.repeat(i) + "</li>"; // returns tree to html
+      document.getElementById("displayTree").innerHTML = htmlResult; // builds line for html
+      console.log(conResult); // renders tree in console
+      i += 2; // symbol counter
+      j -= 2; // spacer counter
+} while (i < modCounter); // loop limiter
+console.log("Loop Limiter: " + modCounter); // **** test data **** displays loop limiter ****
 }
- // var i = 1; i < height + 1; i++;
- //    var j = height;
- //    var line = spacer.repeat(j) + symbol.repeat(i);
- //    console.log(line);
-//
-// function treeBuild(sym, hgt){
-//   console.log("Function arguments: " + sym + " " + hgt); // check to see if obj pass arguments
-//   var line = sym;
-//   var spacer = " ";
-//   var j = hgt;
-//   for(var i = 0; i < hgt; i++){
-//     j -= 1;
-//     console.log(spacer.repeat(j) + line.repeat(i));
-//     tree += "<li>" + line.repeat(i) + "</li>";
-//   }
-//   var result = document.getElementById("displayTree");
-//   result.innerHTML = tree;
-//   console.log("%c MerryChristmas ", "background: green; color: yellow; font-size: x-large; text-align: center");
-// }
-
-// "%c text",
-// "background: green; color: yellow; font-size: x-large; text-align: center");
-
-//If either of the input fields does not have a value in it when the user presses
-//the enter key, or presses the button, then display an alert stating that both
-//fields must have a value.
-
-
-//sort symbols for If not statement....come back to
-//pass symbol and height to an obj (2 key/value pairs)....complete
-//pass that object as argument value to function
-//add event listener to call treeBuild, for enter key while one of the input fields has focus.
-
-/*-------- Thinking Lines Below --------------*/
-
-/*=========== There be Dragons here ===================*/
-// function treeData(symbol, height) {
-//     var symbol = document.getElementById("symbol").value;
-    // if ( symbol.value !== /^[\w&.\-]+$/ ){
-//     //   alert("Please enter one # or @");
-//     //   // return;
-//     // }
-//     var height = document.getElementById("height").value;
-//     // if(height !== "3"||"4"||"5"||"6"||"7"||"8"||"9"||"10"||"11"||"12"||"13"||"14"||"15"){
-//     //   alert("Please enter one number, 3 - 15")
-//     // }
-//     this.symbolTree = symbol;
-//     this.heightTree = height;
-//     console.log("Can you read this? " + symbol + " " + height);
-//     console.log(treeData);
-// }
